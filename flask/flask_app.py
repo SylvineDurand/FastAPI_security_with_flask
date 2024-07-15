4d726adf586b50f21c85b6f5be5b3177b62dfef2
SylvineDurand <durand.sylvine@gmail.com>
2024-07-15 17:09:40 +0200

    add some modifications on flask

--- flask/flask_app.py
+++ flask/flask_app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
 FASTAPI_URL = 'http://localhost:8000'
 
-
+# Authentication 
+# ----------------------------------------------------------------
 # route to log in using a form
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -19,13 +20,12 @@ def login():
         response = requests.post(f'{FASTAPI_URL}/token', data={'username': username, 'password': password})
         if response.status_code == 200:
             token = response.json().get('access_token')
-            resp = redirect('/protected')
+            resp = redirect('/welcome')
             resp.set_cookie('token', token, httponly=True) 
             return resp
         else:
             return jsonify({'error': 'Invalid credentials'}), 401
     return render_template('login.html')
-
 
 
 # log out
@@ -35,6 +35,8 @@ def logout():
     resp.delete_cookie('token') 
     return resp
 
+# Administration
+# ----------------------------------------------------------------
 # admin create a user 
 @app.route('/admin/create_user', methods=['GET', 'POST'])
 def create_user():
@@ -43,7 +45,6 @@ def create_user():
         email = request.form['email']
         full_name = request.form['full_name']
         password = request.form['password']
-        disabled = request.form.get('disabled') == 'on'
         user_role = request.form['user_role']  
         
         user_data = {
@@ -51,23 +52,16 @@ def create_user():
             'email': email,
             'full_name': full_name,
             'password': password,
-            'disabled': disabled,
             'user_role': user_role
         }
         
         response = requests.post(f'{FASTAPI_URL}/create_user/', json=user_data)
         if response.status_code == 200:
-            return redirect(url_for('create_user'))
+            return render_template('user_created_successfully.html', username=username)
         else:
             return jsonify(response.json()), response.status_code
     
     return render_template('create_user.html')
-
-# # admin list of users
-# @app.route('/admin/users_list', methods=['GET'])
-# def get_users_list():
-#     response = requests.get(f'{FASTAPI_URL}/users_list')
-#     return 
 
 @app.route('/admin/users_list')
 def users_list():
@@ -80,10 +74,12 @@ def users_list():
     if response.status_code == 200:
         users = response.json()
         return render_template('users_list.html', users=users)
+    if response.status_code == 403:
+        return render_template('unauthorized.html')
     return jsonify({'error': 'Failed to fetch users'}), response.status_code
 
-
-
+# App 
+# ----------------------------------------------------------------
 # protected route accessible only to authorized users and send back their username
 @app.route('/welcome')
 def protected():
@@ -91,6 +87,7 @@ def protected():
     if not token:
         return redirect('/login')
     headers = {'Authorization': f'Bearer {token}'}
+
     response = requests.get(f'{FASTAPI_URL}/users/me', headers=headers)
     if response.status_code == 200:
         user_data = response.json()
@@ -109,7 +106,7 @@ def protected_admin_only():
     response = requests.get(f'{FASTAPI_URL}/admin_only', headers=headers)
     if response.status_code == 200:
         user = response.json()
-        return render_template('simple_json.html', user=user)
+        return render_template('admin_only.html', user=user)
     if response.status_code == 403:
         return render_template('unauthorized.html')
     
@@ -127,7 +124,7 @@ def protected_admin_ai_only():
     response = requests.get(f'{FASTAPI_URL}/admin_and_ai_only', headers=headers)
     if response.status_code == 200:
         user = response.json()
-        return render_template('simple_json.html', user=user)
+        return render_template('admin_only.html', user=user)
     if response.status_code == 403:
         return render_template('unauthorized.html')
     
